a70dbdfa9018cfe2afbc6826a392f627bcc820c8
Nick Fong <132227268+nfch211@users.noreply.github.com>
2024-09-19 16:45:54 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/Niztore_project/",
+  build: {
+    rollupOptions: {
+      external: ["axios"], // Explicitly mark Axios as an external dependency
+    },
+  },
 });
